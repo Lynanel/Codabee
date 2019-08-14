72f667e0dbd3dea21dd44097f321725ea85f9c98
TRAORE Lionel <traore.lionel@icloud.com>
2019-08-14 21:02:13 +0000

    Ajout de plusieurs storyboard à notre application

--- Codabee/AppDelegate.swift
+++ Codabee/AppDelegate.swift
@@ -13,12 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //Configuration manuelle d'un main stroriboard qui sera lancé lors du démarrage
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        //window?.rootViewController = ViewController()
         return true
     }
 
